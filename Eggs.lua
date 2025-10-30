d84d09883a8b6dd2f1586175477f46d0441b2399
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2025-10-30 22:56:14 +0000

    Create Eggs.lua

---
+++ Eggs.lua
@@ -0,0 +1,28 @@
+local EggsData = {
+    Perm = {
+        "Common Egg",
+        "Spotted Egg",
+        "Iceshard Egg",
+        "Inferno Egg",
+        "Spikey Egg",
+        "Magma Egg",
+        "Crystal Egg",
+        "Lunar Egg",
+        "Void Egg",
+        "Hell Egg",
+        "Nightmare Egg",
+        "Rainbow Egg",
+        "Showman Egg",
+        "Mining Egg",
+        "Cyber Egg",
+        "Neon Egg",
+        "Icy Egg",
+        "Vine Egg",
+        "Lava Egg",
+        "Secret Egg",
+        "Atlantis Egg",
+        "Classic Egg"
+    }
+}
+
+return EggsData
